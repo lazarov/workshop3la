2fc60d966057cb58b5b74de71c8852420bda7250
lazarov <lazarov@gazeta.pl>
2015-06-15 23:15:46 +0200

    modals and some styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def admin?
+    signed_in? && current_user.admin?
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
