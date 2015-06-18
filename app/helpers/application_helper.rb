4865755e6caeb34e1e6331c9bd588f77f93c7a7b
la <lazarov@gazeta.pl>
2015-06-18 22:49:59 +0200

    + flash msg and hidden buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+
+  def bootstrap_flash_class(flash_type)
+     { success: 'alert-success',
+       error:   'alert-danger',
+       alert:   'alert-warning',
+       notice:  'alert-info'
+     }[flash_type.to_sym] || flash_type.to_s
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608133635) do
+ActiveRecord::Schema.define(version: 20150618134253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150608133635) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "products", force: true do |t|
